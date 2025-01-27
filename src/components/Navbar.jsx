d71551406e3f0ba85ade1ceb4765a9808abe4e75
FSFahim <farhanshahriar3728@gmail.com>
2025-01-28 02:15:12 +0600

    All Section Made Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../stylesheets/navbar.css";
 import Button from "./Button";
 import Logo from "./Logo";
 
 const Navbar = ({ darkMode, toggleDarkMode }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <header className={`header ${darkMode ? "darkHeader" : ""}`}>
+    <header
+      className={`header ${darkMode ? "darkHeader" : ""} ${
+        isScrolled ? "scrolled" : ""
+      }`}
+    >
       <a href="/">
         <Logo darkMode={darkMode} />
       </a>
-      <nav className={`navbar ${darkMode ? "darkNavbar" : ""}`}>
-        <a href="#hero">Home</a>
-        <a href="#about-me">About Me</a>
-        <a href="#services">Services</a>
-        <a href="#projects">Projects</a>
-        <a href="#testimonials">Testimonials</a>
-        <a href="#contact">Contact</a>
+      <div
+        className={`menuIcon ${darkMode ? "darkMenuIcon" : ""}`}
+        onClick={toggleMenu}
+      >
+        <i className="fa-solid fa-bars"></i>
+      </div>
+      <nav
+        className={`navbar ${menuOpen ? "open" : ""} ${
+          darkMode ? "darkNavbar" : ""
+        }`}
+      >
+        <a href="#hero" onClick={toggleMenu}>
+          Home
+        </a>
+        <a href="#about-me" onClick={toggleMenu}>
+          About Me
+        </a>
+        <a href="#services" onClick={toggleMenu}>
+          Services
+        </a>
+        <a href="#projects" onClick={toggleMenu}>
+          Projects
+        </a>
+        <a href="#testimonials" onClick={toggleMenu}>
+          Testimonials
+        </a>
+        <a href="#contact" onClick={toggleMenu}>
+          Contact
+        </a>
         <Button buttonText="Download CV" className="navItem" />
         <i
           className={`navItem fa-regular ${
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,8 +5,8 @@ const ProjectCard = ({ project, darkMode }) => {
   return (
     <div className={`projectCard ${darkMode ? "darkProjectCard" : ""}`}>
       <img src={project.image} alt={project.title} className="projectImage" />
-      <h4 className="projectCategory">{project.category}</h4>
-      <h3 className="projectTitle">{project.title}</h3>
+      <div className="projectCategory">{project.category}</div>
+      <div className="projectTitle">{project.title}</div>
     </div>
   );
 };
